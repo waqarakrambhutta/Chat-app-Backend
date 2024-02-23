e240b8b9e0fdb856b33e146edcd3682f98299db2
waqarakrambhutta <waqarakrambhutta@gmail.com>
2024-02-23 14:30:48 +0500

    Extending app for chats

--- jatte/settings.py
+++ jatte/settings.py
@@ -33,6 +33,12 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/chat-admin/'
 LOGOUT_REDIRECT_URL = '/'
 
+CHANNEL_LAYERS={
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer',
+    }
+}
+
 # Application definition
 
 INSTALLED_APPS = [
